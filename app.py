365d95eedfbed4d3a0fdb722173ad507e5c1d5c7
jimmydice <jimmydice@gmail.com>
2024-08-07 20:21:04 +0300

    inital code

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("hello")
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/test', methods=['GET', 'POST'])
+def test_api():
+    if request.method == 'POST':
+        data = request.get_json()
+        return jsonify({"message": "POST request received!", "data": data}), 200
+    else:
+        return jsonify({"message": "GET request received!"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
